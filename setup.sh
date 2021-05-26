ebb59265948bdc489e06cd66e59e151c62f23054
arakaki Tokyo <49378221+arakaki-tokyo@users.noreply.github.com>
2021-05-26 18:30:46 +0900

    Change with organizing files

--- setup.sh
+++ setup.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-cd $(dirname $0)
-cd ../nginx
+WD=$(dirname $0)
+cd ${WD}
+cd confs/nginx
 
 TARGET_DIR="/etc/nginx/sites-enabled/"
 TARGET_FILE="docker.conf" 
-echo ${TARGET_DIR}${TARGET_FILE}
 if [ -L ${TARGET_DIR}${TARGET_FILE} ]; then
 	sudo rm ${TARGET_DIR}${TARGET_FILE}
+	echo "rm ${TARGET_DIR}${TARGET_FILE}"
 fi
 
 sudo ln -s $PWD/docker.conf ${TARGET_DIR}
+echo "ln -s $PWD/docker.conf ${TARGET_DIR}"
 
 # cd ../dnsmasq
 # sudo ln -s $PWD/dnsmasq.conf /etc/

a8ec6e9e271b94f8102ca63cb71dc9d9a75ac5c0
Arakaki Tokyo <49378221+arakaki-tokyo@users.noreply.github.com>
2022-02-08 22:52:47 +0900

    Add init script of samba

--- init/docker_init.sh
+++ init/docker_init.sh
@@ -1,34 +1,46 @@
 #!/bin/bash
-HR1='-------------------------------------------------------------------------\n'
-HR2='=========================================================================\n'
-# Installation
-echo -e '# Installation and Setup Docker!! \n'
-## Set up the repository
+p(){
+  style=${2:-"4;31"}
+  echo -e "\e[${style}m${1}\e[m"
+}
 
-echo -e $HR2 '## Set up the repository'
-echo -e $HR2 
+echo -e '                                                  🐳🐳                                      '
+echo -e '🐳🐳🐳🐳🐳🐳                                      🐳🐳                                      '
+echo -e '🐳🐳      🐳🐳🐳                                  🐳🐳                                      '
+echo -e '🐳🐳          🐳🐳                                🐳🐳                                      '
+echo -e '🐳🐳          🐳🐳      🐳🐳🐳          🐳🐳🐳🐳  🐳🐳    🐳🐳      🐳🐳🐳      🐳    🐳🐳  '
+echo -e '🐳🐳            🐳  🐳🐳🐳  🐳🐳🐳    🐳🐳    🐳  🐳🐳  🐳🐳    🐳🐳🐳  🐳🐳    🐳🐳🐳  🐳🐳'
+echo -e '🐳🐳            🐳  🐳🐳      🐳🐳  🐳🐳          🐳🐳🐳🐳      🐳🐳      🐳🐳  🐳🐳      🐳'
+echo -e '🐳🐳            🐳  🐳          🐳  🐳            🐳🐳🐳        🐳🐳🐳🐳🐳🐳🐳  🐳🐳        '
+echo -e '🐳🐳          🐳🐳  🐳          🐳  🐳            🐳🐳🐳🐳      🐳🐳            🐳🐳        '
+echo -e '🐳🐳          🐳🐳  🐳🐳      🐳🐳  🐳🐳          🐳🐳  🐳🐳    🐳🐳            🐳🐳        '
+echo -e '🐳🐳      🐳🐳🐳    🐳🐳🐳  🐳🐳    🐳🐳🐳    🐳  🐳🐳    🐳🐳  🐳🐳🐳    🐳🐳  🐳🐳        '
+echo -e '🐳🐳🐳🐳🐳🐳          🐳🐳🐳🐳          🐳🐳🐳🐳  🐳🐳      🐳🐳  🐳🐳🐳🐳🐳    🐳🐳        '
+
+# Installation
+p 'Installation and Setup Docker!!' "1;47;41"
+
+p 'Set up the repository\n' 
 sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
 
-echo -e $HR1 '### Add Docker’s official GPG key'
-echo -e $HR1
+p 'Add Docker’s official GPG key\n'
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-echo -e $HR1 '### Set up the stable repository'
-echo -e $HR1
+p 'Set up the stable repository\n'
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 
-echo -e $HR2 '## Install Docker Engine'
-echo -e $HR2
+p 'Install Docker Engine\n'
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
-echo -e $HR2 '## Create docker network'
-echo -e $HR2
+# Configration
+p 'Docker configrations' "1;47;41"
+p 'Create docker network\n'
 docker network create --subnet=172.18.0.0/24 agora
